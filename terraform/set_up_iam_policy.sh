afaccdaf88c2e75a9e886f52c78354f3d5228012
Yuito <yuitosato.w@gmail.com>
2019-03-09 22:44:53 +0900

    :)

---
+++ terraform/set_up_iam_policy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PROJECT_NUMBER="$(gcloud projects describe gcp-yuito-sandbox --format='get(projectNumber)')"
+gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+    --role=roles/container.developer
