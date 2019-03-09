1f1bb4f2f0aac8178a3652f15a6d50cd0c5e9de6
Yuito <yuitosato.w@gmail.com>
2019-03-10 00:32:47 +0900

    deploy

--- nodejs-api/src/app/server.ts
+++ nodejs-api/src/app/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  return res.send('Hello World!');
+  return res.send('Hello World!!!');
 });
 
 app.listen(3000, () => { console.log('Listen to 3000 port...'); });
